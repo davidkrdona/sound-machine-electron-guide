00bc0eca1b8c77fd6fbdab658b58d6f9e415751f
davidkrdona <davidkrdona7@hotmail.es>
2017-08-23 00:29:44 +0200

    info-index.js

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
 'use strict';
 
-var app = require('app');
-var BrowserWindow = require('browser-window');
+const {app, BrowserWindow} = require("electron");
 
 var mainWindow = null;
 
 app.on('ready', function() {
     mainWindow = new BrowserWindow({
-        height: 600,
-        width: 800
+        frame: false,
+        resizable: false,
+        height: 700,
+        width: 370
     });
 
-    mainWindow.loadUrl('file://' + __dirname + '/app/index.html');
+    mainWindow.loadURL('file://' + __dirname + '/app/index.html');
 });
--- readme.md
+++ readme.md
@@ -1,3 +1,10 @@
 # Accompanying repository for the Electron guide
 
 ![Sound Machine](https://rawgithub.com/bojzi/sound-machine/master/sketch/sound-machine.png)
+
+## app/index.js
+query for the sound buttons,
+iterate through the buttons reading out the data-sound attribute,
+add a background image to each button
+and add a click event to each button that plays audio (using the HTMLAudioElement(https://developer.mozilla.org/en/docs/Web/API/HTMLAudioElement) interface)
+(https://developer.mozilla.org/en/docs/Web/API/HTMLAudioElement)
